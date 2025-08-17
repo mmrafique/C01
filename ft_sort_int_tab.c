66f40eb21635fbea2df78c9daa552718b3ba57fc
Muhammad Anas <anasabid607@gmail.com>
2025-08-17 12:15:50 +0200

    C01

--- ft_sort_int_tab.c
+++ ft_sort_int_tab.c
@@ -1,34 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: muhmajee <muhmajee@student.42barcelona.com>  #+#  +:+       +#+        */
+/*   By: muhmajee <muhmajee@student.42barcelona.com>  #+#  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-08-16 22:59:17 by muhmajee          #+#    #+#             */
-/*   Updated: 2025-08-16 22:59:17 by muhmajee         ###   ########.fr       */
+/*   Created: 2025-08-17 10:12:14 by muhmajee          #+#    #+#             */
+/*   Updated: 2025-08-17 10:12:14 by muhmajee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_rev_int_tab(int *tab, int size)
+void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
+	int	j;
 	int	temp;
-	int	flag;
 
 	i = 0;
-	flag = 1;
-	while (flag)
+	while (i < size - 1)
 	{
-		if (i >= size / 2)
-			flag = 0;
-		else
+		j = i + 1;
+		while (j < size)
 		{
-			temp = tab[i];
-			tab[i] = tab[size - i - 1];
-			tab[size - i - 1] = temp;
-			i++;
-			
+			if (tab[i] > tab[j])
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
 		}
 	}
 }
